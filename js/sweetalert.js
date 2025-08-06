49510f4ca7a1cdb3dd1fedbadf03d6d0c43c10a4
emma <eneiradoss@proton.me>
2025-08-06 02:04:23 +0200

    Actualizado avatar, empezado páginas de proyectos y cambios menores

---
+++ js/sweetalert.js
@@ -0,0 +1,13 @@
+Swal.fire({
+    title: 'Ponte el casco!!',
+    text: 'Este sitio está en construcción, hay muchas cosas que pueden cambiar y otras de momento pueden no funcionar.',
+    icon: 'info',
+    confirmButtonText: 'Tomar  casco amarillo',
+    theme: 'dark',
+    background: 'radial-gradient(circle at top, #1b1f3b, #0d0d1a)',
+    allowOutsideClick: true,
+    allowEscapeCLick: true,
+    allowEnterClick: true,
+    confirmButtonColor: '#3a3457',
+    confirmButtonAriaLabel: 'Confirmar',
+});
